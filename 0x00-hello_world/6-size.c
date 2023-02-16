13829cb09b003a2aaf16c7f6806abbcb19962ece
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-02-16 14:20:11 -0800

    made changes to ther variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,20 +5,20 @@
  *
  * Return: always 0 (success)
  *
- **/
+ */
 
 int main(void)
 {
 	int i;
 	char c;
 	float f;
-	long int li;
-	long long int lli;
+	long int w;
+	long long int z;
 	
 	printf("size of a char: %d byte(s) \n", sizeof(c));
 	printf("size of an int: %d byte(s) \n", sizeof(i));
-	printf("size of a long int: %d byte(s) \n", sizeof(li));
-	printf("size of a long long int: %d byte(s) \n", sizeof(lli));
+	printf("size of a long int: %d byte(s) \n", sizeof(w));
+	printf("size of a long long int: %d byte(s) \n", sizeof(z));
 	printf("size of a float: %d byte(s) \n", sizeof(f));
 	return (0);
 }
