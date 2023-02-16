9f0d64faa2f6636ab9f378212e44f5cb72d67d3a
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-02-16 14:13:12 -0800

    changed the text

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	long int li;
 	long long int lli;
 	
-	printf("size of char: %d byte(s) \n", sizeof(c));
-	printf("size of int: %d byte(s) \n", sizeof(i));
-	printf("size of long int: %d byte(s) \n", sizeof(li));
-	printf("size of long long int: %d byte(s) \n", sizeof(lli));
-	printf("size of float: %d byte(s) \n", sizeof(f));
+	printf("size of a char: %d byte(s) \n", sizeof(c));
+	printf("size of an int: %d byte(s) \n", sizeof(i));
+	printf("size of a long int: %d byte(s) \n", sizeof(li));
+	printf("size of a long long int: %d byte(s) \n", sizeof(lli));
+	printf("size of a float: %d byte(s) \n", sizeof(f));
 	return (0);
 }
