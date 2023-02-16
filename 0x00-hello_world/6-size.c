946a90ab97ba6e6c8ab994bfaacdfe930762a5cb
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-02-16 14:08:57 -0800

    changed the sentence size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	float f;
 	long int li;
 	long long int lli;
-
-	printf("size of int is: %d byte(s) \n", sizeof(i));
-	printf("size of char is: %d byte(s) \n", sizeof(c));
-	printf("size of float is: %d byte(s) \n", sizeof(f));
-	printf("size of long int is: %d byte(s) \n", sizeof(li));
-	printf("size of long long int is: %d byte(s) \n", sizeof(lli));
+	
+	printf("size of char: %d byte(s) \n", sizeof(c));
+	printf("size of int: %d byte(s) \n", sizeof(i));
+	printf("size of long int: %d byte(s) \n", sizeof(li));
+	printf("size of long long int: %d byte(s) \n", sizeof(lli));
+	printf("size of float: %d byte(s) \n", sizeof(f));
 	return (0);
 }
