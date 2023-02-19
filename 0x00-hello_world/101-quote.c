e565e7103fee409ba77f591a58836d5c7d15ee6e
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-02-19 01:56:26 -0800

    upgraded the program code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,16 @@
-#include <stdio.h>
 #include <unistd.h>
 
-
-/*
- * main - entry point
- * description - prints exactly and that piece of art
- *		is useful\" - Dora Korpar, 2015-10-19
- * return: 1
- * compile - should show no error with wall
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- *
- *
+ * Return: Always 1.
  */
-
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	59);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
 	return (1);
 }
-
-
