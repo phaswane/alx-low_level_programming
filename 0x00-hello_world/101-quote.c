7a4b4f5c48b289a4faaf4121e3f8312bab87427d
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-02-19 01:46:15 -0800

    updated the comments

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
+
 /*
  * main - entry point
  * description - prints exactly and that piece of art
  *		is useful\" - Dora Korpar, 2015-10-19
  * return: 1
  * compile - should show no error with wall
+ *
+ *
  *
  */
 
