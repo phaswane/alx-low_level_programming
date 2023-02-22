6d3a6ade1b5fcc1e0ee7c4ac305b63b76963538c
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-02-22 11:50:52 -0800

    updated the putcher script

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints _putchar, followed by a new line.
@@ -11,15 +11,15 @@
 int main(void)
 {
 
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
