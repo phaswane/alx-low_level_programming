c8e2a6d57758a2e7ef3aa350e5c352a82c3e6702
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-02-24 13:30:16 -0800

    fizz buzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz test
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
