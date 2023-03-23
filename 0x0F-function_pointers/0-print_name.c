f60bf70036a6b5bfae28649c3b774f5031c73fd8
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-03-23 12:42:49 -0700

    added a NULL conditions to the functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name- this function prints a name
  * @name: string to be printed
@@ -8,5 +9,9 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
 	(*f)(name);
 }
