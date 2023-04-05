bae16b312ab046ee7fd366b01daff98d970a71b3
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-04-05 13:35:24 -0700

    updated the function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,22 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * *_realloc - reallocate memory size function
- * @ptr: pointer to address of old memory location
- * @old_size: unsigned int type of old memory size
- * @new_size: unsigned int type for new memory size
- * Return:  return pointer to array
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to address of old memory
+ * @old_size: unsigned int for old memory size
+ * @new_size: unsigned int for new memory size
+ * Return:  return pointer to an  array
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *s;
+	char *z;
 
 	if (new_size > old_size)
 	{
-		s = malloc(new_size);
+		z = malloc(new_size);
 		free(ptr);
-		return (s);
+		return (z);
 	}
 	if (new_size == old_size)
 	{
@@ -26,9 +26,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		s = malloc(new_size);
+		z = malloc(new_size);
 		free(ptr);
-		return (s);
+		return (z);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
