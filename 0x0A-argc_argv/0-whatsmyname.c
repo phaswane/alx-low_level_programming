df42fac2d0929bb023dae90fa041adb4a4275df4
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-06-09 11:32:59 -0700

    a function print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * c: is the character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include "main.h"
 
 /**
  * main - prints the name of the pragram
