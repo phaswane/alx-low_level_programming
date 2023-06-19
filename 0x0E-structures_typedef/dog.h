309435bba6744df793df6c9c2ffe6916f0fbe16b
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-06-19 11:35:47 -0700

    updated the header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - the name of the structure data type
+ * @name: name of the array of char data type
+ * @age: name of a float variable
+ * @owner: pointer to an array of char data type
+ */
 
 struct dog{
 	char *name;
