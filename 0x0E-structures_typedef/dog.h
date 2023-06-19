b75c886709769a9ee62be7cd6b86571401f44f52
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-06-19 11:48:12 -0700

    added more prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,15 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - new name for struct dog
+ */
+
+typedef struct dog dog_t;i
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
