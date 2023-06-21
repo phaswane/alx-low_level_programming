4db82d311541844ad402185fdd6ed8b187552b96
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-06-21 13:57:42 -0700

    updated the file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,11 +15,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL)
+		return (0);
+	if (cmp == NULL)
+		return (0);
+
 	if (size <= 0)
 		return (-1);
-
-	if (array == NULL || cmp == NULL)
-		return (0);
 
 	for (i = 0; i < size; i++)
 	{
