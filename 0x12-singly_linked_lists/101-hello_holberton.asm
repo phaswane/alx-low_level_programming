5e87d51f28eadac70e79f67fabba564c94fb764e
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-06-29 22:16:10 +0200

    64-bit program in assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+	format db "Hello, Holberton", 0
+
+section .text
+	extern printf
+
+global main
+
+main:
+	sub rsp, 8		;
+
+	mov rdi, format		;
+	xor eax, eax		;
+	call printf		;
+
+	add rsp, 8		;
+	xor eax, eax		;
+	ret
