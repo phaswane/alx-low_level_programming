d143bf59e6f6aace2cdb83fe0efda85ca6ff072e
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-06-29 21:48:29 +0200

    updated the function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,9 +11,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes;
+	size_t nodes = 0;
 
-	for (nodes = 0; h != NULL; nodes++)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
@@ -21,6 +21,8 @@ size_t print_list(const list_t *h)
 		{
 			printf("[%u] %s\n", h->len, h->str);
 		}
+
+		nodes++;
 
 		h = h->next;
 	}
