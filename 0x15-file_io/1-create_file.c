141ef8dd39aa1491d42a3559568ec722a90c5d76
KLEINBOOI MATHAILA <phaswanemathaila@gmail.com>
2023-07-11 12:41:54 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
- * create_file - Creates a file with the specified name and writes the given text content to it.
- * @filename: The name of the file to create.
- * @text_content: The text content to write to the file.
+ * create_file - Creates a file with the specified name and
+ *		writes the given text content to it
+ * @filename: name of the file to create
+ * @text_content: text content to write to the file
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success or -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-        return -1;
+	int file, _len = 0, bytes_read = 0;
+	char *_buffer;
 
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return -1;
-        }
-    }
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-    close(fd);
-    return 1;
+	if (file == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (_len = 0, buffer = text_content; *buffer; buffer++)
+		{
+			_len++;
+		}
+		bytes_read = write(file, text_content, _len);
+	}
+
+	if (close(file) == -1 || _len != bytes_read)
+		return (-1);
+
+	return (1);
 }
