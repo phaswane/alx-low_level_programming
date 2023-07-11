ed50bd3725c705f04b1a2772615e630272165586
KLEINBOOI MATHAILA <phaswanemathaila@gmail.com>
2023-07-11 13:25:21 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (_len = 0, buffer = text_content; *buffer; buffer++)
+		for (_len = 0, _buffer = text_content; *_buffer; _buffer++)
 		{
 			_len++;
 		}
